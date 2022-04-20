ddd0ecc9568b9a5686f7ab09d7ef64fcf2a6e633
mvkth <104092012+mvkth@users.noreply.github.com>
2022-04-21 01:22:44 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ from source.nodemap import NodeMap
 from source.graphtools import Graph, Path
 from source.weathertools import WeatherTools, driver
 
-points = GeoTools.filterWater(GeoTools.makeMatrix(step=2.0))
+points = GeoTools.filterWater(GeoTools.makeMatrix(step=0.25))
 neighbors = GeoTools.getNeighbors(points)
 weather = WeatherTools.getWeather(points)
 
 nodes = NodeMap.generate_nodes(points, neighbors, weather)
 # NodeMap.show_nodes(nodes)
 
-PlotTools.plotPH()
-PlotTools.plotEdges(neighbors)
-PlotTools.plotPoints(points)
-PlotTools.showPlot()
-
 graph = Graph(nodes)
 # graph.show_edges()
 paths = graph.dijkstra(points[0][0])
-path = Path.generate_path(paths,(124.0, 9.0))
+
+PlotTools.plotPH()
+PlotTools.plotEdges(neighbors)
+PlotTools.plotPoints(points)
+
+path = Path.generate_path(paths,(122.0, 12.0))
 for i in path:
     a , b = i
     PlotTools.plot_segment(a,b,'#ffbb00')
+
+PlotTools.set_xlim(119,123)
+PlotTools.set_ylim(9.5,13.0)
+PlotTools.showPlot()
 driver.quit()
